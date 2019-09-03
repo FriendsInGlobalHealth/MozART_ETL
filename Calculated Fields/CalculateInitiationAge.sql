b732af36a34cb31abd681ecc4dfbf2de49aebf6c
Randy Yee <PCX5@cdc.gov>
2019-09-03 12:24:18 +0200

    Add files via upload

---
+++ Calculated Fields/CalculateInitiationAge.sql
@@ -0,0 +1,15 @@
+﻿DROP PROCEDURE IF EXISTS [dbo].[CalculateInitiationAge]
+GO
+
+ALTER TABLE [dbo].[t_paciente] DROP COLUMN IF EXISTS [CalculateInitiationAge]
+GO
+
+CREATE PROCEDURE [dbo].[CalculateInitiationAge]
+
+AS
+
+UPDATE t_paciente
+SET idade = CASE WHEN datainiciotarv IS NULL OR datanasc IS NULL THEN idade
+ELSE (CONVERT(int,CONVERT(char(8),cast(datainiciotarv as date),112))-CONVERT(char(8),cast(datanasc as date),112))/10000
+END;
+
