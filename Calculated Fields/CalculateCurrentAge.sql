8053cf7e3e2ecb094f8b9e15776f3e93d5670032
Randy Yee <PCX5@cdc.gov>
2019-09-03 12:24:44 +0200

    Add files via upload

---
+++ Calculated Fields/CalculateCurrentAge.sql
@@ -0,0 +1,16 @@
+﻿DROP PROCEDURE IF EXISTS [dbo].[CalculateCurrentAge]
+GO
+
+ALTER TABLE [dbo].[t_paciente] DROP COLUMN IF EXISTS [Current_Age]
+GO
+
+CREATE PROCEDURE [dbo].[CalculateCurrentAge]
+
+AS
+
+ALTER TABLE [dbo].[t_paciente] ADD [Current_Age] 
+    AS CASE WHEN datanasc IS NULL THEN NULL
+ELSE (CONVERT(int,CONVERT(char(8),GetDate(),112))-CONVERT(char(8),cast(datanasc as date),112))/10000
+END
+
+
